4323eb5d187d95da4f80d8a931de72ebbc5812cd
111kunal <kunalahirwar111@gmail.com>
2025-09-08 23:28:06 +0530

    Added function.js file

---
+++ function.js
@@ -0,0 +1,33 @@
+
+// function intro(name , age){
+//     return `hello my name is ${name}. and i am ${age} years old`;
+// }
+
+// console.log(intro('kunal',24))\
+
+// let i = 23;
+
+
+// function increment(a){
+//     a++;
+//     console.log(a);
+// }
+
+
+// console.log(increment(23));
+// console.log(i);
+
+//using function with variable
+
+// let x = function(a,b){
+//     return a+b;
+// }
+
+// let c = x(3,4);
+// console.log(c);
+
+//function with arrow 
+
+// let sum = (a,b) => a+b;
+
+// console.log(sum(4,3));
