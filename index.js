71943ce60ff834a3e456ed8ff7c2b8d527e1c849
111kunal <kunalahirwar111@gmail.com>
2025-09-08 23:38:46 +0530

    updating index.js file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(false || false || false);
+let firstname = 'kunal';
+
+// console.log(firstname.toUpperCase());
+// console.log(firstname.substring(0,3));
+
+// for(let i=0;i<firstname.length;i++){
+//     console.log(firstname[i])
+// }
+let sentence = "what is your name and where are you from";
+let words = sentence.split(' ');
+console.log(words);
+
+console.log(words.join('<'))
