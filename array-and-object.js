931de10d756a27a7f037fedd97cd3100aabd48b4
111kunal <kunalahirwar111@gmail.com>
2025-09-09 23:45:00 +0530

    commiting code to array file

--- array-and-object.js
+++ array-and-object.js
@@ -12,3 +12,36 @@
 // console.log(person.name);
 // console.log(person.brother);
 // console.log(person.country)
+
+// const student = {
+//     name:"kunal",
+//     age:21,
+//     marks:{maths:23,
+//         english:34,
+//         hindi:34
+//     }
+// }
+
+// console.log(student.name);
+// console.log(student.age);
+
+// student.grade="A";
+
+// student.marks.english=65;
+
+// console.log(student.marks.english);
+
+// function average(){
+
+// }
+//object constructor function
+
+// function student(name,age,marks){
+//     this.name = name;
+//     this.age = age;
+//     this.marks = marks;
+// }
+
+
+const person1 = new person("kunal",21,23);
+const person2 = new person("vinay",22,23);
