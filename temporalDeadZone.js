80bfd8621a4cf3f5466736e48c4cca7fa30b7c13
111kunal <kunalahirwar111@gmail.com>
2025-09-27 14:54:59 +0530

    adding new fiels and learning things

---
+++ temporalDeadZone.js
@@ -0,0 +1,26 @@
+//funcion scoping 
+// function sayhi() {
+//     var fullname = "Lydia";
+//     console.log(fullname);
+// }
+
+// sayhi();
+
+//block scoping
+// {
+//     var fullname = "Kunal";
+//     console.log(fullname);
+// }
+// console.log(fullname);
+
+//global scoping 
+// let fullname = "kunal";
+// {
+//     console.log(fullname,"inside block");
+// }
+
+// function greet(){
+//     console.log(fullname,"inside function");
+// }
+
+// greet();
--- variable-function-hosting.js
+++ variable-function-hosting.js
@@ -1,5 +1,31 @@
-print("kunal");
+//variable hosting 
+//using var 
+//while using var it will give us undefined because it is only hosting the declaration and not the initialization 
 
-function print(firstname){
-    console.log(firstname);
-}
+// console.log(a);
+
+// var a = 5;
+
+
+//variable hosting using let and const
+
+// function greetme(greet,fullname){
+//     console.log("hello",fullname);
+//     greet();
+// }
+
+// function greet(){
+//     console.log("hello kunal");
+// }
+
+// greetme(greet,"kunal ahirwar");
+
+// function solve(number){
+//     return function(number){
+//         return number*number;
+//     }
+// }
+
+
+// let ans =solve(5);
+// console.log(ans(5));
