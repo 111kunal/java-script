1fe782625e84bfb378e606c6d1ed9cd91ae0b456
111kunal <kunalahirwar111@gmail.com>
2025-10-26 21:56:18 +0530

    learnign dom manupulation for html and css

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let firstname = 'kunal';
+// let firstname = 'kunal';
 
-// console.log(firstname.toUpperCase());
-// console.log(firstname.substring(0,3));
+// // console.log(firstname.toUpperCase());
+// // console.log(firstname.substring(0,3));
 
-// for(let i=0;i<firstname.length;i++){
-//     console.log(firstname[i])
-// }
-let sentence = "what is your name and where are you from";
-let words = sentence.split(' ');
-console.log(words);
+// // for(let i=0;i<firstname.length;i++){
+// //     console.log(firstname[i])
+// // }
+// let sentence = "what is your name and where are you from";
+// let words = sentence.split(' ');
+// console.log(words);
 
-console.log(words.join('<'))
+// console.log(words.join('<'))
+
+let a = document.querySelector('#name');
+a.innerHTML = 'hello my name is vinay';
+
+let element = document.createElement('h1');
+element.textContent = 'hello how are you';
+
+let bodytag = document.querySelector('body');
+bodytag.insertAdjacentElement('afterbegin',element);
