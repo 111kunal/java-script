f3675f23f7662ef3f0cad4f4cb2f07714c8652aa
111kunal <kunalahirwar111@gmail.com>
2025-10-28 23:20:19 +0530

    managing the files

--- lecture1/index.js
+++ lecture1/index.js
@@ -1,13 +1,22 @@
- function color() {
-    let element =document.getElementById("spara");
-    element.style.background = "gray";
- }
- function color1() {
-    let element =document.getElementById("tpara");
-    element.style.background = "black";
- }
+// let firstname = 'kunal';
 
- function color3() {
-    let element =document.getElementById("tpara");
-    element.style.background = "red";
- }
+// // console.log(firstname.toUpperCase());
+// // console.log(firstname.substring(0,3));
+
+// // for(let i=0;i<firstname.length;i++){
+// //     console.log(firstname[i])
+// // }
+// let sentence = "what is your name and where are you from";
+// let words = sentence.split(' ');
+// console.log(words);
+
+// console.log(words.join('<'))
+
+let a = document.querySelector('#name');
+a.innerHTML = 'hello my name is vinay';
+
+let element = document.createElement('h1');
+element.textContent = 'hello how are you';
+
+let bodytag = document.querySelector('body');
+bodytag.insertAdjacentElement('afterbegin',element);
---
+++ lecture1/jsDOM-css-elements.js
@@ -0,0 +1,13 @@
+ function color() {
+    let element =document.getElementById("spara");
+    element.style.background = "gray";
+ }
+ function color1() {
+    let element =document.getElementById("tpara");
+    element.style.background = "black";
+ }
+
+ function color3() {
+    let element =document.getElementById("tpara");
+    element.style.background = "red";
+ }
